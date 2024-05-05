b59693b6be38b43ca287dcec7ed778440c4fd252
= <nguyenvannam14056969@gmail.com>
2024-05-06 01:22:43 +0700

    add

--- train.py
+++ train.py
@@ -1,19 +1,30 @@
+import logging
+import math
 import os
+import datasets
 import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
+import transformers
+from accelerate import Accelerator
+from accelerate.logging import get_logger
+from accelerate.utils import ProjectConfiguration, set_seed
 from transformers import CLIPTextModel, CLIPTokenizer
+from transformers.utils import ContextManagers
+import diffusers
 from diffusers import AutoencoderKL, DDPMScheduler, UNet2DConditionModel
 from diffusers.optimization import get_scheduler
 from diffusers.utils import is_wandb_available
-from utils import load_config
+from tqdm import tqdm
+from utils import load_config, deepspeed_zero_init_disabled_context_manager
 from models.ldm import LatenFashionDIFF
 from dataset import DataFASSHIONDIFF
 import time
-from torch import autocast 
+
 if is_wandb_available():
     import wandb
-import math
+logger = get_logger(__name__, log_level="INFO")
+
 ## config global
 path_config  = "./config.yaml"
 config = load_config(path_config)
@@ -37,17 +48,17 @@ def load_models(config):
 
     
     
-    text_encoder = CLIPTextModel.from_pretrained(
-        config.pretrained_model_name_or_path, subfolder="text_encoder", revision=config.revision, variant=config.variant
-    ).eval()
-    vae = AutoencoderKL.from_pretrained(
-        config.pretrained_model_name_or_path, subfolder="vae", revision=config.revision, variant=config.variant
-    ).eval()
+    with ContextManagers(deepspeed_zero_init_disabled_context_manager()):
+        text_encoder = CLIPTextModel.from_pretrained(
+            config.pretrained_model_name_or_path, subfolder="text_encoder", revision=config.revision, variant=config.variant
+        )
+        vae = AutoencoderKL.from_pretrained(
+            config.pretrained_model_name_or_path, subfolder="vae", revision=config.revision, variant=config.variant
+        )
 
     unet = UNet2DConditionModel.from_pretrained(
         config.pretrained_model_name_or_path, subfolder="unet", revision=config.non_ema_revision
     )
-    
     #text_encoder, vae, unet, process_diffusion, scaling_factor
     model = LatenFashionDIFF(
         text_encoder = text_encoder,
@@ -63,16 +74,50 @@ def collate_fn(examples):
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
         input_ids = torch.stack([example["input_ids"] for example in examples])
         return {"pixel_values": pixel_values, "input_ids": input_ids}
-    
-def main():
 
+def main():
+    
+
+    logging_dir = os.path.join(config.output_dir, config.logging_dir)
+
+    accelerator_project_config = ProjectConfiguration(project_dir=config.output_dir, logging_dir=logging_dir)
+
+    accelerator = Accelerator(
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        mixed_precision=config.mixed_precision,
+        log_with=config.report_to,
+        project_config=accelerator_project_config,
+    )
+#     return accelerator
+
+    # Disable AMP for MPS.
+    if torch.backends.mps.is_available():
+        accelerator.native_amp = False
+
+    # Make one log on every process with the configuration for debugging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state, main_process_only=False)
+    if accelerator.is_local_main_process:
+        datasets.utils.logging.set_verbosity_warning()
+        transformers.utils.logging.set_verbosity_warning()
+        diffusers.utils.logging.set_verbosity_info()
+    else:
+        datasets.utils.logging.set_verbosity_error()
+        transformers.utils.logging.set_verbosity_error()
+        diffusers.utils.logging.set_verbosity_error()
 
     # If passed along, set the training seed now.
     if config.seed is not None:
-        torch.manual_seed(config.seed)
+        set_seed(config.seed)
 
-    if config.output_dir is not None:
-        os.makedirs(config.output_dir, exist_ok=True)
+    # Handle the repository creation
+    if accelerator.is_main_process:
+        if config.output_dir is not None:
+            os.makedirs(config.output_dir, exist_ok=True)
 
 
     model, tokenizer = load_models(config)
@@ -113,6 +158,9 @@ def main():
     )
 
 
+
+    
+    
     train_dataset = DataFASSHIONDIFF(
         path_meta = config.data['train'],
         size= config.data['size'],
@@ -145,107 +193,167 @@ def main():
             batch_size=config.train_batch_size,
     )
     
-    lr_scheduler  = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda x: 1/math.sqrt(x + 0.001)) 
 
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # Scheduler and math around the number of training steps.
+    overrode_max_train_steps = False
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / config.gradient_accumulation_steps)
     
+    if config.max_train_steps is None:
+        config.max_train_steps = config.num_train_epochs * num_update_steps_per_epoch
+        overrode_max_train_steps = True
+
+    lr_scheduler = get_scheduler(
+        config.lr_scheduler,
+        optimizer=optimizer,
+        num_warmup_steps=config.lr_warmup_steps * accelerator.num_processes,
+        num_training_steps=config.max_train_steps * accelerator.num_processes,
+    )
+
+    # Prepare everything with our `accelerator`.
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
+
+    # For mixed precision training we cast all non-trainable weights (vae, non-lora text_encoder and non-lora unet) to half-precision
+    # as these weights are only used for inference, keeping weights in full precision is not required.
+    weight_dtype = torch.float32
+    if accelerator.mixed_precision == "fp16":
+        weight_dtype = torch.float16
+        config.mixed_precision = accelerator.mixed_precision
+    elif accelerator.mixed_precision == "bf16":
+        weight_dtype = torch.bfloat16
+        config.mixed_precision = accelerator.mixed_precision
+
     # Move text_encode and vae to gpu and cast to weight_dtype
-    model.text_encoder.to(device)
-    model.vae.to(device)
-    model.to(device)
+    model.text_encoder.to(accelerator.device, dtype=weight_dtype)
+    model.vae.to(accelerator.device, dtype=weight_dtype)
+
+    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / config.gradient_accumulation_steps)
+    
+    if overrode_max_train_steps:
+        config.max_train_steps = config.num_train_epochs * num_update_steps_per_epoch
+        
+    # Afterwards we recalculate our number of training epochs
+    config.num_train_epochs = math.ceil(config.max_train_steps / num_update_steps_per_epoch)
+
+    # We need to initialize the trackers we use, and also store our configuration.
+    # The trackers initializes automatically on the main process.
+    # if accelerator.is_main_process:
+    #     tracker_config = dict(vars(config))
+    #     # print(tracker_config)
+    #     # tracker_config.pop("validation_prompts")
+    #     accelerator.init_trackers(config.tracker_project_name, tracker_config)
+
+
+    print("Running training")
+
+    logger.info("***** Running training *****")
+    logger.info(f"  Num examples = {len(train_dataset)}")
+    logger.info(f"  Num Epochs = {config.num_train_epochs}")
+    logger.info(f"  Instantaneous batch size per device = {config.train_batch_size}")
+    logger.info(f"  Gradient Accumulation steps = {config.gradient_accumulation_steps}")
+    logger.info(f"  Total optimization steps = {config.max_train_steps}")
     global_step = 0
-    first_epoch = 0  
+    first_epoch = 0
+
+    initial_global_step = 0
+    
+    progress_bar = tqdm(
+        range(0, config.max_train_steps),
+        initial=initial_global_step,
+        desc="Steps",
+        # Only show the progress bar once on each machine.
+        disable=not accelerator.is_local_main_process,
+    )
+    
+    
     
     min_loss = None
     start_time = time.time()
 
-    if config.path_checkpoint:
-        checkpoint = torch.load(config.path_checkpoint)
-
-        first_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
-        config.num_train_epochs += first_epoch
-        print(f"Load model: {config.path_checkpoint}")
-        
-    for epoch in range(first_epoch, config.num_train_epochs):
+    for epoch in tqdm(range(first_epoch, config.num_train_epochs)):
         end_time = time.time()
         training_time = end_time - start_time
-        max_training_time = 10 * 3600  
+        max_training_time = 11 * 3600  
         if training_time > max_training_time:
             print("Training time exceeded 11 hours. Stopping training...")
-            break
                 
         
         
         model.train()
         train_loss = 0.0
-        test_loss = 0.0
-        step_counts = 0
-        for step, batch in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            # Convert images to latent space
-            batch["pixel_values"] =batch["pixel_values"].to(device)
-            batch["input_ids"] =batch["input_ids"].to(device).long()
-
-            target, model_pred = model(pixel_values = batch["pixel_values"], input_ids = batch["input_ids"])
-
-            loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean") / config.gradient_accumulation_steps
-
-            train_loss += loss.item() 
-
-            # Backpropagate
-            loss.backward()
-            
-            
-            
-            if (step + 1) % int(config.gradient_accumulation_steps) == 0:
-                step_counts +=1                
-                torch.nn.utils.clip_grad_norm_(model.parameters(), config.max_grad_norm)
-                optimizer.step()
+        for step, batch in tqdm(enumerate(train_dataloader), total = len(train_dataloader)):
+            with accelerator.accumulate(model):
+                # Convert images to latent space
+                batch["pixel_values"] =batch["pixel_values"].to(accelerator.device).to(weight_dtype)
+                batch["input_ids"] =batch["input_ids"].to(accelerator.device).to(weight_dtype).long()
                 
+                
+                
+                # Predict the noise residual and compute loss
+                target, model_pred = model(pixel_values = batch["pixel_values"], input_ids = batch["input_ids"])
+
+                loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+ 
+
+                # Gather the losses across all processes for logging (if we use distributed training).
+                avg_loss = accelerator.gather(loss.repeat(config.train_batch_size)).mean()
+                train_loss += avg_loss.item() / config.gradient_accumulation_steps
+
+                # Backpropagate
+                accelerator.backward(loss)
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_norm_(model.parameters(), config.max_grad_norm)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+            progress_bar.set_postfix(**logs)
+
+           
             global_step+=1
-        lr_scheduler.step()
         
         model.eval()
-        
-        for step, batch in enumerate(test_dataloader):
-            # Convert images to latent space
-            batch["pixel_values"] =batch["pixel_values"].to(device)
-            batch["input_ids"] =batch["input_ids"].to(device).long()
-            
-            # Predict the noise residual and compute loss
-            target, model_pred = model(pixel_values = batch["pixel_values"], input_ids = batch["input_ids"])
+        test_loss = 0.0
+        for step, batch in tqdm(enumerate(test_dataloader), total = len(test_dataloader)):
+            with accelerator.accumulate(model):
+                # Convert images to latent space
+                batch["pixel_values"] =batch["pixel_values"].to(accelerator.device).to(weight_dtype)
+                batch["input_ids"] =batch["input_ids"].to(accelerator.device).to(weight_dtype).long()
+                
+                
+                
+                # Predict the noise residual and compute loss
+                target, model_pred = model(pixel_values = batch["pixel_values"], input_ids = batch["input_ids"])
 
-            loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
-            
-            test_loss += loss.item() / config.gradient_accumulation_steps
-    
+                loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+ 
+
+                # Gather the losses across all processes for logging (if we use distributed training).
+                avg_loss = accelerator.gather(loss.repeat(config.train_batch_size)).mean()
+                test_loss += avg_loss.item() / config.gradient_accumulation_steps
+
+       
+
+         
         
         train_loss = round(train_loss/len(train_dataloader), 4)
         test_loss = round(test_loss/len(test_dataloader), 4)
 
+        accelerator.log({"train_loss": train_loss}, step=global_step)
         wandb.log(
             {
                 
                 'Train loss': train_loss, 
-                'Test loss': test_loss,
-                'lr': lr_scheduler.get_last_lr()[0],
-                'step_counts': step_counts
+                'Test loss': test_loss
             }
         )
-        total_loss = train_loss*0.3 + test_loss*0.7
-        if min_loss == None or total_loss <= min_loss:
-            print("Update model")
-            save_path = os.path.join(config.output_dir, f"best.pth")
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': lr_scheduler.state_dict(),
-                }, save_path)
-            min_loss = total_loss
+        if min_loss == None or train_loss <= min_loss:
+            save_path = os.path.join(config.output_dir, f"best")
+            accelerator.save_state(save_path)
+            min_loss = train_loss
             print("Save model")
             image_eval = []
             model.set_up()
@@ -255,15 +363,16 @@ def main():
                 image_eval.append(image)
             wandb.log({"images": image_eval})
         
+        logger.info(f"Saved state to {save_path}")
         print({
                 'epoch':epoch, 
                 'Train loss': train_loss, 
-                'Test loss': test_loss,
-                
+                'Test loss': test_loss
             })
 
         train_loss = 0.0
-        test_loss = 0.0
+    accelerator.end_training()
+
 
 if __name__ == "__main__":
     main()
