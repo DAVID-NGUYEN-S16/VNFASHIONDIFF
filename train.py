d996e626f03b4d1c7f3bebbdf1595ee1ba2c22fa
= <nguyenvannam14056969@gmail.com>
2024-05-06 02:21:49 +0700

    add

--- train.py
+++ train.py
@@ -377,7 +377,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
     # mp.set_start_method('spawn')
-    # notebook_launcher(main, args=(), num_processes=2)
+    print(torch.cuda.is_initialized())
+    notebook_launcher(main, args=(), num_processes=2)
 
