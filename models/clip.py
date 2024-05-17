acab25a8ddd517e3a1a28065c6ca7357d264d82d
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-18 05:38:51 +0700

    adam8bit

--- models/clip.py
+++ models/clip.py
@@ -5,8 +5,8 @@ class VNCLIP_model(nn.Module):
     def __init__(self, name_model = 'M-CLIP/XLM-Roberta-Large-Vit-L-14'):
         super().__init__()
         self.model = pt_multilingual_clip.MultilingualCLIP.from_pretrained(name_model)
-        self.text_encoder = self.transformer
-        self.linear_proj = self.LinearTransformation
+        self.text_encoder = self.model.transformer
+        self.linear_proj = self.model.LinearTransformation
     def forward(self, inputs):
         '''
         Return last_hidden_state from input
