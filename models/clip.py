70dfcdd6eff6e089a37645342af4478c435e7a52
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-18 23:47:32 +0700

    update-infer

--- models/clip.py
+++ models/clip.py
@@ -15,7 +15,7 @@ class VNCLIPEncoder(nn.Module):
         
             inputs: (B, S, D)
         '''
-        
+        print(input_ids.size)
         inp = self.text_encoder(input_ids,attention_mask = attention_mask ).last_hidden_state
         inp = self.linear_proj(inp)
         return [inp]
