856be986b72fb1f1b73e9515a78fad276aa628fc
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-18 15:16:18 +0700

    new-text-encoder

--- train.py
+++ train.py
@@ -418,5 +418,5 @@ if __name__ == "__main__":
     # main()
     # mp.set_start_method('spawn')
     print(torch.cuda.is_initialized())
-    notebook_launcher(main, args=(), num_processes=2)
+    notebook_launcher(main, args=(), num_processes=1)
 
