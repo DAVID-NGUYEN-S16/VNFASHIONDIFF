e4579a8618bdcb0724c639f251cef6c14cc6cd71
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-18 12:50:53 +0700

    new-text-encoder

--- train.py
+++ train.py
@@ -259,7 +259,7 @@ def main():
     # model.module.vae.to(accelerator.device, dtype=weight_dtype)
     accelerator.unwrap_model(model).text_encoder.to(accelerator.device, dtype=weight_dtype)
     accelerator.unwrap_model(model).vae.to(accelerator.device, dtype=weight_dtype)
-    print(device)
+    print(accelerator.device)
     model.to(device)
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / config.gradient_accumulation_steps)
