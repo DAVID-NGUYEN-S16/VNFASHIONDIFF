efe2369a5fa7cff5aabae2fbab05e5c0948a6f0d
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-18 12:48:35 +0700

    new-text-encoder

--- models/ldm.py
+++ models/ldm.py
@@ -25,6 +25,7 @@ class LatenFashionDIFF(nn.Module):
         self.tokenizer = tokenizer
         self.set_up()
     def forward(self, pixel_values, input_ids, attention_mask):
+        print(pixel_values.size())
         latents = self.vae.encode(pixel_values).latent_dist.sample()
         latents = latents * self.scaling_factor
         # create distribution of latens        
