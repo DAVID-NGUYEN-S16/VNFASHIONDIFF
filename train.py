d3aebef22186b5fcbe3261d43a74b481865f35b2
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-19 23:41:57 +0700

    descrease-batch-size

--- train.py
+++ train.py
@@ -369,5 +369,5 @@ def main():
 if __name__ == "__main__":
     # main()
     print(torch.cuda.is_initialized())
-    notebook_launcher(main, args=(), num_processes=2)
+    notebook_launcher(main, args=(), num_processes=1)
 
