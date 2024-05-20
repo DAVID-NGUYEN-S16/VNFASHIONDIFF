82b476535033498cacbc67dd743ff5622005c8f7
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-21 00:16:54 +0700

    add-ddp

--- models/ldm.py
+++ models/ldm.py
@@ -64,16 +64,16 @@ class LatenFashionDIFF(nn.Module):
         
         if noisy_latents.device != target_device:
             print("noisy_latents")
-            noisy_latents.to(target_device)
+            noisy_latents = noisy_latents.to(target_device)
             
         if timesteps.device != target_device:
             print("timesteps")
-            timesteps.to(target_device)
+            timesteps = timesteps.to(target_device)
         
         
         if encoder_hidden_states.device != target_device:
             print("encoder_hidden_states")
-            encoder_hidden_states.to(target_device)
+            encoder_hidden_states = encoder_hidden_states.to(target_device)
         print(encoder_hidden_states.device, timesteps.device, noisy_latents.device)
         print(self.model.device)
         model_pred = self.model(x = noisy_latents, time_steps = timesteps, context = encoder_hidden_states)[0]
