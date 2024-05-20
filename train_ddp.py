add02142d817cc8b78756679f96bd7b663670915
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-20 18:37:32 +0700

    add-ddp

--- train_ddp.py
+++ train_ddp.py
@@ -205,7 +205,7 @@ def train(gpu_id, world_size, rank):
         # Clean memory
         torch.cuda.empty_cache()
         gc.collect()
-    # model.to(gpu_id)
+    model.to(gpu_id)
     model = DDP(model, device_ids=[gpu_id])
 
     model = setting_compute_model(config=config, model=model)
